fef6931d011b65282a2af5eb9efd747417c034ad
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-10-09 17:16:15 +0200

    Added controller for new book

--- app/controllers/new-book.js
+++ app/controllers/new-book.js
@@ -1,11 +1,33 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class NewBookController extends Controller {
-  @tracked title = '';
+  @service store;
+  @service router;
 
-  printData = (input) => {
-    this.title = input.target.value;
-    console.log(this.title);
-  };
+  @action
+  updateField(field, event) {
+    this.newBook[field] = event.target.value;
+  }
+
+  @action
+  async addBook() {
+    try {
+      let book = this.store.createRecord('book', {
+        title: this.newBook.title,
+        genre: this.newBook.genre,
+        pages: Number(this.newBook.pages),
+        language: this.newBook.language,
+        publisher: this.newBook.publisher,
+        date: new Date(this.newBook.date),
+        isbn: this.newBook.isbn,
+      });
+
+      await book.save();
+      this.router.transitionTo('books');
+    } catch (e) {
+      console.error('Error saving book:', e);
+    }
+  }
 }
--- app/routes/new-book.js
+++ app/routes/new-book.js
@@ -1,3 +1,21 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class NewBookRoute extends Route {}
+export default class NewBookRoute extends Route {
+  @service store;
+  @service router;
+
+  setupController(controller) {
+    super.setupController(...arguments);
+
+    controller.newBook = {
+      title: '',
+      genre: '',
+      pages: '',
+      language: '',
+      publisher: '',
+      date: '',
+      isbn: '',
+    };
+  }
+}
