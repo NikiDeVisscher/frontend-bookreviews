60b11ae3ac8f45864b70c1e0b452bfcb150030a8
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-10-14 09:39:58 +0200

    Added form validation

--- app/controllers/edit-book.js
+++ app/controllers/edit-book.js
@@ -7,6 +7,7 @@ export default class EditBookController extends Controller {
   @service router;
 
   @tracked published = this.formatDate(this.model.date);
+  @tracked errors = {};
 
   languages = [
     'English',
@@ -26,18 +27,61 @@ export default class EditBookController extends Controller {
   }
 
   @action
+  updateField(field, event) {
+    this.model[field] = event.target.value;
+  }
+
+  @action
   async saveBook() {
     try {
+      if (!this.validate()) {
+        return;
+      }
+
       await this.model.save();
       this.router.transitionTo('book', this.model.id);
     } catch (error) {
       console.error('Error saving book:', error);
-      alert('Failed to save book.');
     }
   }
 
   @action
   cancel() {
     this.router.transitionTo('book', this.model.id);
+  }
+
+  validate() {
+    this.errors = {};
+
+    if (!this.model.title) {
+      this.errors.title = 'Title is required';
+    }
+    /*if (!this.selectedAuthors.length) {
+      this.errors.authors = 'At least one author is required';
+    }*/
+    if (!this.model.genre) {
+      this.errors.genre = 'Genre is required';
+    }
+    if (!this.model.pages || isNaN(Number(this.model.pages))) {
+      this.errors.pages = 'Pages must be a number';
+    }
+    if (!this.model.language) {
+      this.errors.language = 'Language is required';
+    }
+    if (!this.model.publisher) {
+      this.errors.publisher = 'Publisher is required';
+    }
+    if (!this.model.date) {
+      this.errors.date = 'Date is required';
+    }
+    if (!this.model.isbn) {
+      this.errors.isbn = 'ISBN is required';
+    }
+
+    if (Object.keys(this.errors).length) {
+      return false;
+    }
+
+    return true;
   }
 }
