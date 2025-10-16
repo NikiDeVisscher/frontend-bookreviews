b617e030bcef5178fd9f28bc24b14c19c71f9918
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-10-16 10:53:21 +0200

    Loading reviews

--- app/components/reviews.js
+++ app/components/reviews.js
@@ -1,6 +1,3 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 
-export default class Reviews extends Component {
-  @tracked reviews = this.args.book.reviews || [];
-}
+export default class Reviews extends Component {}
