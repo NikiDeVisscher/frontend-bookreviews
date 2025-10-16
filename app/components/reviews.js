c724431955987fb09b8fdb5a4e8f3c317edcaed0
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-10-16 10:17:43 +0200

    Added review model

--- app/components/reviews.js
+++ app/components/reviews.js
@@ -1,3 +1,6 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default class Reviews extends Component {}
+export default class Reviews extends Component {
+  @tracked reviews = this.args.book.reviews || [];
+}
