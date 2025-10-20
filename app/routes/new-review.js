664e1c380f71dfec900eb6e1aa4716b53b30ecc4
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-10-20 10:18:09 +0200

    added controller

--- app/routes/new-review.js
+++ app/routes/new-review.js
@@ -1,3 +1,21 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class NewReviewRoute extends Route {}
+export default class NewReviewRoute extends Route {
+  @service store;
+  @service router;
+
+  setupController(controller) {
+    super.setupController(...arguments);
+
+    controller.newReview = {
+      reviewcontent: '',
+      reviewrating: null,
+      book: this.store.createRecord('book', {
+        id: this.router.currentRoute.params.id,
+      }),
+      datecreated: new Date(),
+      author: null,
+    };
+  }
+}
