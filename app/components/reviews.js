02557d0f68187197ace3b25be9b159ae25110ffc
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-11-13 14:36:02 +0100

    Show own review

--- app/components/reviews.js
+++ app/components/reviews.js
@@ -1,9 +1,39 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class Reviews extends Component {
   @service router;
+  @service currentUser;
+
+  @tracked otherReviews = [];
+  @tracked userReviews = [];
+
+  constructor() {
+    super(...arguments);
+    this.loadUserReviews();
+  }
+
+  async loadUserReviews() {
+    const currentUser = this.currentUser.user;
+    const reviews = await this.args.book.reviews;
+    for (const review of reviews) {
+      await review.account;
+    }
+
+    if (currentUser) {
+      this.userReviews = reviews.filter(
+        (review) => review.account.id === currentUser.id,
+      );
+
+      this.otherReviews = reviews.filter(
+        (review) => review.account.id !== currentUser.id,
+      );
+    } else {
+      this.otherReviews = this.args.book.reviews;
+    }
+  }
 
   @action
   editReview(review) {
