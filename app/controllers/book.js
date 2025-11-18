d558d5194246349735bf1a47947982f9ada1a360
NikiDeVisscher <niki.de.visscher@gmail.com>
2025-11-18 10:34:27 +0100

    Improved ux based on suggestions

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -1,44 +1,18 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class BookController extends Controller {
   @service router;
   @service session;
   @service currentUser;
 
+  @tracked hasUserReview = false;
+
   @action
-  async navigateAddReview(event) {
-    event.preventDefault();
-
-    if (this.session.isAuthenticated) {
-      if (await this.checkUserReview()) {
-        alert('You have already submitted a review for this book.');
-        return;
-      }
-
-      this.router.transitionTo('new-review', this.model.id);
-    } else {
-      this.router.transitionTo('login');
-    }
-  }
-
-  async checkUserReview() {
-    const reviews = await this.model.reviews;
-    for (let review of reviews) {
-      await review.account;
-    }
-
-    const currentUser = this.currentUser.user;
-    let hasUserReview = false;
-
-    if (currentUser) {
-      hasUserReview = reviews.some(
-        (review) => review.account?.id === currentUser.id,
-      );
-    }
-
-    return hasUserReview;
+  handleReviewExists(exists) {
+    this.hasUserReview = exists;
   }
 
   @action
